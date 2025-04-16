7cacb3bc839fac0c8ceba43f3b366b58eedc74a4
mohasinakonda <mohasinakonda39@gmail.com>
2025-04-16 22:52:58 +0600

    chore: add meta data

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,6 +1,11 @@
 import { getAllPosts } from "@/lib/api";
 import ClientBlogPage from "./client-page";
-
+import { Metadata } from "next";
+export const metadata: Metadata = {
+	title: "Blog",
+	description: "",
+	authors: [{ name: "Hazrat akonda" }],
+};
 export default async function BlogPage() {
 	const posts = getAllPosts();
 	return <ClientBlogPage initialPosts={posts} />;
