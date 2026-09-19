b380d2d23095b46c686c9f37808472a286f6d026
cronixweb-hazrat <hazrat.akonda@cronixweb.com>
2026-09-20 00:04:11 +0600

    feat: update hero, footer, and blog post components

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -2,6 +2,9 @@
 import Link from "next/link";
 import { motion } from "framer-motion";
 import { BugEaterText } from "./title-masking";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { FiMail } from "react-icons/fi";
 
 const fadeUp = (delay = 0) => ({
 	initial: { opacity: 0, y: 14 },
@@ -123,7 +126,7 @@ export const Hero = () => {
 								aria-label="GitHub"
 								className="text-[#525252] hover:text-[#EDEDED] transition-colors duration-150"
 							>
-								<GithubIcon />
+								<FaGithub size={18} />
 							</Link>
 							<Link
 								href="https://www.linkedin.com/in/hazrat-ali-akonda/"
@@ -132,7 +135,7 @@ export const Hero = () => {
 								aria-label="LinkedIn"
 								className="text-[#525252] hover:text-[#EDEDED] transition-colors duration-150"
 							>
-								<LinkedInIcon />
+								<FaLinkedin size={18} />
 							</Link>
 							<Link
 								href="https://x.com/mohasinakonda1"
@@ -141,14 +144,14 @@ export const Hero = () => {
 								aria-label="X"
 								className="text-[#525252] hover:text-[#EDEDED] transition-colors duration-150"
 							>
-								<XIcon />
+								<FaXTwitter size={18} />
 							</Link>
 							{/* <Link
 								href="mailto:mohasinakonda@gmail.com"
 								aria-label="Email"
 								className="text-[#525252] hover:text-[#EDEDED] transition-colors duration-150"
 							>
-								<MailIcon />
+								<FiMail size={18} />
 							</Link> */}
 						</motion.div>
 					</div>
@@ -159,36 +162,3 @@ export const Hero = () => {
 		</section>
 	);
 };
-
-/* Flat Monochrome SVGs */
-function GithubIcon() {
-	return (
-		<svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
-			<path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
-		</svg>
-	);
-}
-
-function LinkedInIcon() {
-	return (
-		<svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
-			<path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
-		</svg>
-	);
-}
-
-function MailIcon() {
-	return (
-		<svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
-			<rect x="2" y="4" width="20" height="16" rx="0" />
-			<path d="M2 4l10 9 10-9" />
-		</svg>
-	);
-}
-
-const XIcon = () => {
-	return (
-		<svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} fill="currentColor" viewBox="0 0 1024 1024"><path fillRule="evenodd" d="M823.111 912H200.89C151.8 912 112 872.2 112 823.111V200.89C112 151.8 151.8 112 200.889 112H823.11C872.2 112 912 151.8 912 200.889V823.11C912 872.2 872.2 912 823.111 912M286 291L457.772805 536.277658L286 735H331.296885L477.865892 564.968959L596.942 735H740L555.891306 472.107691L715 289H670.330417L535.793156 443.40916L429.058 291zm69.053 37.65 258.935 368.699h56.96L412.013 328.65h-56.96z" />
-		</svg>
-	);
-}
